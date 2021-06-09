e7ee6867c823045eeea87eb546d9977b1c100dd9
Sorob Louie <sorob@sorob.com>
2021-06-09 10:03:58 -0400

    adding meta files to the project and also the global time class

---
+++ DisguiseUnityRenderStream/DisguiseGlobalTime.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Events;
+using System;
+
+public class DisguiseGlobalTime : MonoBehaviour
+{
+    public UnityAction<float> onTimeUpdate;
+
+    void Update()
+    {
+        if (DisguiseRenderStream.newFrameData)
+        {
+
+
+            if (onTimeUpdate != null)
+            {
+               onTimeUpdate((float)DisguiseRenderStream.frameData.localTime);
+            }
+
+        }
+    }
+
+
+}
